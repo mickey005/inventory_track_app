8960b99f8a1be5b72c733bb16cd4f45f5880116e
gauravgupta566 <guptagaurav9566@gmail.com>
2017-04-20 17:51:58 +0530

    commented code

--- app/src/main/java/com/example/android/inventroytrack/MainActivity.java
+++ app/src/main/java/com/example/android/inventroytrack/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.android.inventroytrack;
 
-import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+      /*  final FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,DetailsActivity.class);
                 startActivity(intent);
             }
-        });
+        });*/
     }
 
     @Override
